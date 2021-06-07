cd156a01736b1e2cfb6cac1e5532b1e7e24cebd3
elunacado <54047744+elunacado@users.noreply.github.com>
2021-06-06 21:53:04 -0500

    A player controller and a check ground

---
+++ PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed= 2;
+    public float jumpSpeed= 3;
+
+    Rigidbody2D rb2d;
+
+    void Start(){
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        if(Input.GetKey("d")||Input.GetKey("right"))
+        {
+            rb2d.velocity= new Vector2(runSpeed, rb2d.velocity.y);
+        }
+        else if(Input.GetKey("a")||Input.GetKey("left"))
+        {
+            rb2d.velocity= new Vector2(-runSpeed, rb2d.velocity.y);
+
+        }
+        
+        else
+        {
+            rb2d.velocity=new Vector2(0, rb2d.velocity.y);
+        }
+
+        if(Input.GetKey("w")&& checkGnd.isGrounded)
+        {
+            rb2d.velocity= new Vector2(rb2d.velocity.x, jumpSpeed);
+
+        }
+    }
+}
---
+++ checkGnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkGnd : MonoBehaviour
+{
+    public static bool isGrounded;
+    
+    
+
+    private void OnTriggerEnter2D(Collider2D col) {
+           if (col.gameObject.tag == "gnd")
+           {
+            isGrounded = true;
+               
+           }
+    }
+    
+    private void OnTriggerExit2D(Collider2D col) {
+            isGrounded = false;
+    }
+}
